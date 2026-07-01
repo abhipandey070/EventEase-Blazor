0349b17c3d3e693a0f73347323b7bd879401bd36
Abhishek Pandey <abhipandey7050@gmail.com>
2026-07-01 13:39:21 +0530

    Update Event.cs

--- models/Event.cs
+++ models/Event.cs
@@ -1,1 +1,10 @@
-
+namespace EventEase.Models
+{
+    public class Event
+    {
+        public string Name { get; set; } = "";
+        public string Location { get; set; } = "";
+        public string Date { get; set; } = "";
+        public int RegisteredUsers { get; set; }
+    }
+}
