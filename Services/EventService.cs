b7b3bd333d2c65c05fc01147a3549e1aeccd1ff7
Abhishek Pandey <abhipandey7050@gmail.com>
2026-07-01 13:41:27 +0530

    Update EventService.cs

--- Services/EventService.cs
+++ Services/EventService.cs
@@ -1,1 +1,27 @@
+using System.Collections.Generic;
+using EventEase.Models;
 
+namespace EventEase.Services
+{
+    public class EventService
+    {
+        public List<Event> Events { get; set; } = new List<Event>
+        {
+            new Event
+            {
+                Name="Microsoft Build",
+                Location="Online",
+                Date="June 2026",
+                RegisteredUsers=50
+            },
+
+            new Event
+            {
+                Name="AI Workshop",
+                Location="VIT",
+                Date="July 2026",
+                RegisteredUsers=120
+            }
+        };
+    }
+}
